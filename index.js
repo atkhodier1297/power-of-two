671e79bc4662abdcf7021dfcd291dfaa17501bec
Adam Khodier <khodieradam1297@gmail.com>
2024-02-19 11:13:57 -0500

    bitwise solution constant time

--- index.js
+++ index.js
@@ -18,3 +18,15 @@ console.log(powerOfTwo(8))
 console.log(powerOfTwo(27))
 
 // Anytime you reduce input size by half...Big O === O(logn)
+
+function isPowerOfTwoBitWise(n){
+    if(n < 1){
+        return false
+    }
+    return (n & (n-1)) === 0
+}
+
+// bitwise solution checks the binary form of the integer to see of it ends in 0.
+// if it does not then it is not a power of two.
+// 4 in binary is 1000 so it is a power of two.
+// 5 is not a power of two because its binary number is 101

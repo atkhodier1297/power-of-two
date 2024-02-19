66c05a1128320693b9aed4ef89b3dd39ed0ca2ad
Adam Khodier <khodieradam1297@gmail.com>
2024-02-19 11:04:00 -0500

    solved

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 // Problem: Determine if the integer 'n' is a power of two.
 // Repeatedly divide 'n' by 2 and if the remainder is always 0 then its a power of 2.
+
+function powerOfTwo(n){
+    if(n < 1){
+        return false
+    }
+    while(n > 1){
+        if(n % 2 !== 0){
+            return false
+        }
+        n = n / 2
+    }
+    return true
+}
+
+console.log(powerOfTwo(8))
+console.log(powerOfTwo(27))
